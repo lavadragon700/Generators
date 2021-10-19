df507f2f7c944f652b9f66f228bdcafcd43c4c2d
lavad <lavadog700@gmail.com>
2021-10-19 09:03:07 -0400

    The start

--- src/main/java/me/lavadragon700/generators/Generators.java
+++ src/main/java/me/lavadragon700/generators/Generators.java
@@ -4,9 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Generators extends JavaPlugin {
 
+    public static Generators plugin;
+
     @Override
-    public void onEnable() {
-        // Plugin startup logic
+    public void onEnable()
+    {
+        plugin = this;
+        this.saveDefaultConfig();
 
     }
 
---
+++ src/main/java/me/lavadragon700/generators/commands/GeneratorCmd.java
@@ -0,0 +1,14 @@
+package me.lavadragon700.generators.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class GeneratorCmd implements CommandExecutor
+{
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
