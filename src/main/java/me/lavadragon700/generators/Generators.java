5047940a42f92cd36832c909d6820c3ad3f2ca4a
lavad <lavadog700@gmail.com>
2021-10-19 08:43:14 -0400

    The start

---
+++ src/main/java/me/lavadragon700/generators/Generators.java
@@ -0,0 +1,17 @@
+package me.lavadragon700.generators;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Generators extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
